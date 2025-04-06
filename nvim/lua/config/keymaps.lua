6830dccc18f928b0ef4a5e9de649e94982de7996
quochuy242 <truongquochuy.240203@gmail.com>
2025-04-06 08:01:41 +0700

    update neovim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local key = vim.keymap
+
+-- Split window
+key.set("n", "ss", ":split<Return>", { noremap = true, silent = true })
+key.set("n", "sv", ":vsplit<Return>", { noremap = true, silent = true })
+
+-- Move window
+key.set("n", "<Up>", "<c-w>k")
+key.set("n", "<Down>", "<c-w>j")
+key.set("n", "<Left>", "<c-w>h")
+key.set("n", "<Right>", "<c-w>l")
+
+-- Copy and paste the current line below
+key.set("n", "yp", "yyp", { noremap = true, silent = true })
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+        presets = { inc_rename = true },
+      })
+    end,
+  },
+  {
+    "akinsho/nvim-bufferline.lua",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = { options = { separator_style = "slope" } },
+  },
+}
